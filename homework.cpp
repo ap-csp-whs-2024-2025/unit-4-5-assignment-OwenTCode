bf5556294861f71e02bd585a1f420d12d1e35a5a
OwenTCode <Oweneight88@gmail.com>
2025-01-09 22:37:06 +0000

    done

--- homework.cpp
+++ homework.cpp
@@ -8,8 +8,31 @@ void displayList(std::vector<T> const &v);
 int main()
 {
     // You can follow along with any code in class here
+    // Task 1
+    std::cout << "Task 1\n";
+    std::vector<int> Integers = {1, 2, 3, 4, 5};
+    std::vector<double> Double = {1.1, 2.2, 3.3, 4.4, 5.5};
+    std::vector<std::string> Strings = {"one", "two", "three", "four", "five"};
+    std::cout << "Lists exist now\n";
 
-    
+    // Task 2
+    std::cout << "Task 2\n";
+    std::cout << "Index 1 is: " << Integers[1] << std::endl;
+    std::cout << "Index 3 is: " << Integers[3] << std::endl;
+    std::cout << "Index 5 is: " << Integers[5] << std::endl;
+
+    // Task 3
+    std::cout << "Task 3\n";
+    displayList(Strings);
+    Strings.push_back("Owen");
+    displayList(Strings);
+
+    // Task 4
+    std::cout << "Task 4\n";
+    displayList(Double);
+    Double.pop_back();
+    displayList(Double);
+
     return 0;
 }
 
